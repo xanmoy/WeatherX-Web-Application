d4ac5e818dc8f960671db2e413f41d7246738dd1
Tanmoy Gangopadhyay <tanmoygangulyofficial@outlook.com>
2023-02-27 15:57:04 +0530

    updated js fixed info issues

--- components/js/script.js
+++ components/js/script.js
@@ -5,22 +5,19 @@ const options = {
 		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
 	}
 };
-const getWeather = (city) => {
+const getWeather = (city)=>{
 	cityName.innerHTML = city
 	fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
 		.then(response => response.json())
 		.then(response => {
 			console.log(response)
 			// cloud_pct.innerHTMl = response.cloud_pct
-			temp.innerHTML = response.temp
-			temp2.innerHTML = response.temp
 			feels_like.innerHTML = response.feels_like
 			humidity.innerHTML = response.humidity
-			humidity2.innerHTML = response.humidity
+			temp.innerHTML = response.temp
 			min_temp.innerHTML = response.min_temp
 			max_temp.innerHTML = response.max_temp
 			wind_speed.innerHTML = response.wind_speed
-			wind_speed2.innerHTML = response.wind_speed
 			wind_degrees.innerHTML = response.wind_degrees
 			sunrise.innerHTML = response.sunrise
 			sunset.innerHTML = response.sunset
